ebf1676a95c03f0ea91f5e1678d743a67d0a0435
chrissydc <bychriscelle@gmail.com>
2019-03-16 11:19:48 -0400

    voice is now recognized and window types it to show on page

---
+++ app.js
@@ -0,0 +1,36 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+
+// for the window to keep typing what we're saying until we stop speaking
+recognition.interimResults = true;
+
+// if I stop speaking, the browser creates another paragraph 
+
+let p = document.createElement("p");
+const words = document.querySelector(".words");
+words.appendChild(p);
+
+recognition.addEventListener("result", e => {
+    console.log(e.results);
+    const transcript = Array.from(e.results)
+        .map(result => result[0]) 
+        .map(result => result.transcript)  // map over transcript to return result
+        .join('');
+
+    p.textContent = transcript;
+
+    if (e.results[0].isFinal) {
+        p = document.createElement("p");
+        words.appendChild(p);
+    }
+
+    if (transcript.includes("unicorn")) {
+        console.log("hihihi")
+    }
+
+    console.log(transcript);
+});
+
+// initialize microphone access to build transcript
+recognition.start();
